ddd6bfcbb192015b59b59885595e0eb3ab97acbe
Sơn Nguyễn <is.thaison@gmail.com>
2020-10-19 22:37:12 +0700

    zzzz

--- App.tsx
+++ App.tsx
@@ -6,6 +6,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>This is the section updates!</Text>
       <StatusBar style="auto" />
     </View>
   );
